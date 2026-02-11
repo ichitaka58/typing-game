fad8ceb68e91cc6de69fac52b07d9ba5ffc4641a
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2026-02-11 21:37:13 +0900

    feat: add code to app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,100 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  const questions = [
+    { question: "React", image: "/monster1.jpg" },
+    { question: "TypeScript", image: "/monster2.jpg" },
+    { question: "JISOU", image: "/monster3.jpg" },
+    { question: "GitHub", image: "/monster4.jpg" },
+    { question: "Next.js", image: "/monster5.jpg" },
+  ];
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+  const [currentPosition, setCurrentPosition] = useState<number>(0);
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+
+  const [isStarted, setIsStarted] = useState<boolean>(false);
+  const [userName, setUserName] = useState<string>("");
+
+  useEffect(() => {
+    const handleKeyDown = async (e: keyboardEvent) => {
+      const currentQuestion = questions[currentQuestionIndex];
+      if (
+        e.key.toLowerCase() ===
+        currentQuestion.question[currentPosition].toLowerCase()
+      ) {
+        setCurrentPosition((prev) => prev + 1);
+      }
+
+      if (currentPosition === currentQuestion.question.length - 1) {
+        if (currentQuestionIndex === questions.length - 1) {
+          setIsCompleted(true);
+        } else {
+          setCurrentQuestionIndex((prev) => prev + 1);
+          setCurrentPosition(0);
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [currentPosition, currentQuestionIndex]);
+
+  const handleStart = () => {
+    if (!userName) {
+      alert("名前を入力してください");
+      return;
+    }
+    setIsStarted(true);
+  };
+
+  if (!isStarted) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black">
+        <div className="text-center p-8">
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Enter your name..."
+            className="w-64 p-3 text-lg bg-amber-50"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div>
+          <button onClick={handleStart} className="px-8 py-3 text-xl bg-red-900">Start Game</button>
         </div>
       </main>
-    </div>
+    );
+  }
+
+  if (isCompleted) {
+    return <div>ゲーム終了</div>;
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div
+        className="text-center w-full h-screen bg-cover bg-center flex flex-col items-center justify-center"
+        style={{
+          backgroundImage: `url(${questions[currentQuestionIndex].image})`,
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          backgroundBlendMode: "overlay",
+        }}
+      >
+        <div>
+          {questions[currentQuestionIndex].question
+            .split("")
+            .map((char, index) => (
+              <span
+                key={index}
+                style={{ color: index < currentPosition ? "#ff0000" : "white" }}
+              >
+                {char}
+              </span>
+            ))}
+        </div>
+      </div>
+    </main>
   );
 }
